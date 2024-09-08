613fc976aa883038d7f4dacfe070dc308f670f19
Cláudio Más <claudio.mas@gmail.com>
2024-09-08 11:11:07 -0300

    resolução técnica 2)

---
+++ tecnica-2.py
@@ -0,0 +1,15 @@
+# Técnica 2)
+# Linguagem: Python
+
+def fibonacci(n):
+    a, b = 0, 1
+    while b < n:
+        a, b = b, a + b
+    return b == n
+
+
+numero = int(input("Informe um número: "))
+if fibonacci(numero):
+    print(f"O número {numero} pertence à sequência de Fibonacci.")
+else:
+    print(f"O número {numero} não pertence à sequência de Fibonacci.")
